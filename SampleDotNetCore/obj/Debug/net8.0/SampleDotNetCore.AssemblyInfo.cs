1056c5df1b13c97738083986fb895297417f3725
Shriansh Kulkarni <shrianshkulkarni12@gmail.com>
2025-08-25 15:53:52 +0530

    Add : DB APPROCHES

--- SampleDotNetCore/obj/Debug/net8.0/SampleDotNetCore.AssemblyInfo.cs
+++ SampleDotNetCore/obj/Debug/net8.0/SampleDotNetCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SampleDotNetCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d19128cfa728d24ddfc791068324a7dae210c92")]
 [assembly: System.Reflection.AssemblyProductAttribute("SampleDotNetCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SampleDotNetCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
